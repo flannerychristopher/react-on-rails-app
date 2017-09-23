b1578b327e0c5e3b557dc079b703d72fd155b553
flan the dev <flanthedev@gmail.com>
2017-09-23 12:57:49 +0800

    remove content component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 
 import UsersIndex from './components/users_index';
-import Content from './components/content';
+import { Route, Link } from 'react-router-dom';
+import UsersShow from './components/users_show';
+import PostsIndex from './components/posts_index';
+import Welcome from './components/welcome';
+import About from './components/about';
+import Flanthedev from './components/flanthedev';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <UsersIndex />
-        <Content />
+        <div className="content">
+          <nav>
+            <Link to="/">Welcome Page</Link>
+            <Link to="/posts">View All Posts</Link>
+            <Link to="/about">About this App</Link>
+            <Link to="/flanthedev">Made by flanthedev</Link>
+          </nav>
+          <h1>The React on Rails App</h1>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/posts" component={PostsIndex} />
+          <Route path="/about" component={About} />
+          <Route path="/flanthedev" component={Flanthedev} />
+          <Route path="/users/:id" component={UsersShow} />
+        </div>
       </div>
     );
   }
